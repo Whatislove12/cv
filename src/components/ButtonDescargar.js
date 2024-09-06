bdd8e332faf45d0f913bff3b666feadba5e4ef9a
Whatislove12 <vlad_podymskiy@mail.ru>
2024-09-06 20:19:09 +0200

    Change CV URL and date of the current job

--- src/components/ButtonDescargar.js
+++ src/components/ButtonDescargar.js
@@ -3,13 +3,25 @@ import { useTranslation } from 'react-i18next';
 
 
 export const ButtonDescargar = () => {
-    const  {t} = useTranslation();
+    const { t, i18n } = useTranslation();
 
     const descargar = () => {
         const link = document.createElement("a");
-        link.href = "/files/CV-VladislavPodymskiy.pdf";
+
+        // Получение текущего языка
+        const currentLanguage = i18n.language;
+
+        // Определение ссылки в зависимости от языка
+        if (currentLanguage === "es") {
+            link.href = "/files/cv-vladislav-podymskiy-es.pdf";
+        } else if (currentLanguage === "en") {
+            link.href = "/files/cv-vladislav-podymskiy-en.pdf";
+        } else {
+            // Значение по умолчанию (например, для других языков)
+            link.href = "/files/CV-VladislavPodymskiy.pdf";
+        }
+
         link.download = "CV-VladislavPodymskiy";
-    
         link.click();
     }
     return (
